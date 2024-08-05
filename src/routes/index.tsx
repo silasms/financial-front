d7874c51bffb14a27deee141e8fe98fb9aef6892
silasms <silas.medeiros7@gmail.com>
2024-08-05 09:36:08 -0300

    feat: auth guard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login";
 import { Register } from "../pages/register";
+import { AuthGuard } from "../guard/auth.guard";
 
 export function Router() {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
+        <Route element={<AuthGuard isPrivate={false} />}>
+          <Route path="/" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+        </Route>
+        <Route element={<AuthGuard isPrivate={true} />}>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
