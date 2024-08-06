e07f9e38a771d08c392bd78048ce8e9670a02d8b
silasms <silas.medeiros7@gmail.com>
2024-08-06 11:45:24 -0300

    style: import style for toastify

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import { ToastContainer } from 'react-toastify'
 import './App.css'
 import { Router } from './routes'
 import { AuthProvider } from './contexts/auth.context'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <AuthProvider>
         <Router />
-        <ToastContainer />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </AuthProvider>
     </>
   )
